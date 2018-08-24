aecb2c9296deed6f20ab95a2736a7591da8b52f8
Jacob Liberman <jliberma@redhat.com>
2018-08-23 23:39:08 -0400

    update compute node nic templates

--- scripts/overcloud-deploy.sh
+++ scripts/overcloud-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-time exec openstack overcloud deploy \
-        --templates /usr/share/openstack-tripleo-heat-templates \
-	-e /home/stack/templates/global-config.yaml \
-        --environment-directory /home/stack/templates/environments
+exec openstack overcloud deploy \
+  --templates /usr/share/openstack-tripleo-heat-templates \
+  --timeout 90 \
+  --verbose \
+  -e /home/stack/templates/global-config.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  --environment-directory /home/stack/templates/environments
+  --log-file /home/stack/overcloud-deploy.log
